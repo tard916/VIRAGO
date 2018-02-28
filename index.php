84d86cebb9b043c9b1b00d7d233dc09a57a56f89
Rahimi Diallo <tard916@outlook.com>
2018-02-28 09:42:18 +0800

    First Push
    first push including Home, sign up and sin in page front-end part.

---
+++ index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="rec/logo/favicon_symbol.png" type="image/png">
+
+    <title>Virago</title>
+
+    <!--This is the google Arvo font and font-awesome-->
+    <link href="https://fonts.googleapis.com/css?family=Arvo" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/custome.css" rel="stylesheet"> 
+
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+   
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand " href="#"><span>  VIRAGO</span></a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li ><a href="#wo_we_are">About us</a></li>
+            <li><a href="#get-involved">How you can Help</a></li>
+            <li><a href="#get-involved">Donation</a></li>
+            <li><a href="signUp.php">SIGN UP</a></li>
+            <li><a href="login.php">SIGN IN</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron">
+      <div class="container">
+        <h1>Our goal is to inspire <br/> Young programmers to write better codes.</h1>
+        <p>We belive that  the <strong> Success is no accident, </strong> it can be reached only <br/>through <strong> Hard Work, perseverance and learning.</strong> We will provide you with the basement to get you started.</p>
+        <p><a class="btn btn-danger btn-lg" href="info.php" role="button">Learn more &raquo;</a></p>
+        <h1 style="text-align: center;">224 Coding</h1>
+      </div>
+    </div>
+    <div id="wo_we_are">
+      <div class="container home-page">
+        <img src="rec/logo/logo.png" alt="224 coding" title="224 Coding Logo">
+        <h2>Who We Are</h2>
+        <hr>
+        <p>
+          We are <a href="http://www.224tech.com" target="_blank"> 224 Tech</a>, We aim to improve seducation throught both method - effective project-based learning - and material - by teaching skills that are applicable to inproving your life today.
+        </p>
+      </div>
+    </div>
+    <div id="get-involved">
+      <div class="container home-page">
+        <!-- Example row of columns -->
+        <h2>Get Involved</h2>
+        <hr>
+        <div class="row">
+          <div class="col-md-4">
+            
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x"></i>
+              <i class="fa fa-male fa-stack-1x fa-inverse"></i>
+            </span>
+            <h3>STUDENTS</h3>
+            <p>Want to learn how to code? Want to help us get to 1,000,000 lines? Click the button below and we'll let you know how to get involved.</p>
+            <p><a class="btn btn-default" href="#" role="button">Start Learning &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x"></i>
+              <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
+            </span>
+            <h3>EDUCATOR</h3>
+            <p>Want to bring this initiative to your school or institution? Awesome! Click the button below and we'll let make it happen.</p>
+            <p><a class="btn btn-default" href="#" role="button">Join The Initiative &raquo;</a></p>
+         </div>
+          <div class="col-md-4">
+            <span class="fa-stack fa-4x">
+              <i class="fa fa-circle fa-stack-2x"></i>
+              <i class="fa fa-money fa-stack-1x fa-inverse"></i>
+            </span>
+            <h3>SPONSORS</h3>
+            <p>We love all the support we get to help host more events, and empower more lives with the knoledge of coding. Click to get involved.</p>
+            <p><a class="btn btn-default" href="#" role="button">Give Support &raquo;</a></p>
+          </div>
+         
+        </div>
+        </div> <!-- /container -->
+      </div>
+
+     
+
+      <footer>
+        <div class="container">
+          <span><img src="rec/logo/favicon.png" width="75px;" height="75px;"></span><p class="pull-right">&copy; 2017 224 Coding</p> 
+
+        </div>
+        
+      </footer>
+    
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <!-- This is the header part with include the link to the CSS -->
+  <meta charset="UTF-8">
+  <title>Sign-IN</title>
+  <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+  <link rel="stylesheet" href="css/styleSignUp.css">
+
+  
+</head>
+
+<body>
+
+  <div class="form">
+      
+      <ul class="tab-group">
+        <li class="tab active login"><a href="#signup">SIGN IN</a></li>
+        
+      </ul>
+      
+      <div class="tab-content">
+        <div id="signup">   
+         
+          <form action="/" method="post">         
+
+            <div class="field-wrap">
+              <label>
+                Email <span class="req">*</span>
+              </label>
+              <input type="email" required autocomplete="off"/>
+            </div>
+            
+            <div class="field-wrap">
+              <label>
+                Password<span class="req">*</span>
+              </label>
+              <input type="password"required autocomplete="off"/>
+            </div>            
+            
+            <button type="submit" class="button button-block"/>SIGN IN</button>
+            <a href="index.php" class="button btn-cancel ">CANCEL</a>
+
+          </form>
+
+          <hr>
+          <span class="Fpass">Forgot password?<a href="#">Click Here</a></span>
+          <span class="Nuser">New User?<a href="signUp.php">Click Here</a></span>
+          
+
+        </div>
+        
+        <div id="login">          
+
+        </div>
+        
+      </div><!-- tab-content -->
+      
+</div> <!-- /form -->
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script  src="js/indexSignUp.js"></script>
+</body>
+
+</html>
