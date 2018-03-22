a55a7665ed0492d089a280a890d4176a1eb8024c
emmagine79 <emmagine79@gmail.com>
2018-03-22 17:39:31 +0800

    Gallery and Carousell
    Editing of Gallery as well as editing the links in the Carousell

--- gallery.php
+++ gallery.php
@@ -2,26 +2,26 @@
 <script src="js/main.js" type="text/javascript"></script>
 
 <div class="example" id="gall">
-    <img src="images/pic1.jpg"><span>Picture 1 title<br>and description.</span> 
-    <img src="images/pic2.jpg"><span>Picture 2 description.</span> 
-    <img src="images/pic3.jpg"><span>Picture 3 description.</span> 
-    <img src="images/pic4.jpg"><span>Picture 4 description.</span> 
-    <img src="images/pic5.jpg"><span>Picture 5 description.</span> 
-    <img src="images/pic6.jpg"><span>Picture 6 description.</span> 
-    <img src="images/pic7.jpg"><span>Picture 7 description.</span> 
-    <img src="images/pic8.jpg"><span>Picture 8 description.</span> 
-    <img src="images/pic9.jpg"><span>Picture 9 description.</span> 
-    <img src="images/pic10.jpg"><span>Picture 10 description.</span> 
-    <img src="images/pic11.jpg"><span>Picture 11 description.</span> 
-    <img src="images/pic12.jpg"><span>Picture 12 description.</span> 
-    <img src="images/pic13.jpg"><span>Picture 13 description.</span> 
-    <img src="images/pic14.jpg"><span>Picture 14 description.</span> 
-    <img src="images/pic15.jpg"><span>Picture 15 description.</span> 
+    <img src="images/pic1.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic2.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic3.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic4.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic5.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic6.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic7.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic8.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic9.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic10.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic11.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic12.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic13.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic14.jpg"><span>Jinjang Utara Community Photos</span>
+    <img src="images/pic15.jpg"><span>Jinjang Utara Community Photos</span>
 </div>
 
 <div style="bottom:0;position:fixed;">
 <hr style="clear:both;" />
 <h4>
-    <a href="index.php">back to the Home Page</a>
+    <a href="info.php">Return to Info Page</a>
 </h4>
 </div>
